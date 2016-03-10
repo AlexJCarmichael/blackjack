68742ccc5445fc67fea29caa89bf6381d56317c0
Dane Carmichael <carmichaeldane@gmail.com>
2016-03-10 09:01:55 -0600

    "Turning in explorer mode, needs a couple of tweaks"

--- bin/blackjack.rb
+++ bin/blackjack.rb
@@ -13,6 +13,8 @@ def dance_with_em
   choice = gets.chomp.upcase
   if choice == "PLAY"
     puts "We playin'"
+    @busted_status = "no"
+    @computer_busted = "no"
   elsif choice == "QUIT"
     puts "Good-bye"
     exit
@@ -73,12 +75,14 @@ def check_blackjack(hand, string)
   hand.each do |x|
     amount += x.display_value
   end
-  if amount == 21
+  if amount == 21 || amount == 22
     puts "Blackjack!"
     @go_again = "no"
     puts "#{string} has a Blackjack!"
     if string == "Player"
       @player_black_jack = "yes"
+    elsif string == "Computer"
+      @computer_black_jack = "yes"
     end
   end
 end
@@ -86,22 +90,34 @@ end
 def dealer_logic(hand, deck)
   if @player_black_jack == "yes"
     @dealer_score = 0
+  elsif @computer_black_jack == "yes"
+    puts "Dealer Wins!"
+    give_space
   else
-    print "Dealer: "
-    hand.each do |x|
-      print  "#{x.display_rank} #{x.suit} "
-    end
+    print "*" * 20
     puts
+    print "Dealer's turn!"
+    puts
+    print "*" * 20
+    puts
+    print "Dealer: "
+    display_hand(hand)
     check_blackjack(hand, "Computer")
+    give_space
     amount = calc_hand(hand)
+    print "Dealer hand: "
+    display_hand(hand)
+    give_space
     while amount < 16
       card = hand.push(deck.draw_card)
-      hand.each do |x|
-        print  "#{x.display_rank} #{x.suit} "
-      end
+      puts "Dealer hits!"
+      give_space
+      display_hand(hand)
+      give_space
       amount = calc_hand(hand)
     end
-      @dealer_score = amount
+    puts "Dealer stands."
+    @dealer_score = amount
     if amount < 22
       puts "Dealer has #{amount}"
       @dealer_score = amount
@@ -117,13 +133,18 @@ def dealer_logic(hand, deck)
 end
 
 def show_dealer_hand(hand)
-  puts "Dealer hand is..."
+  print "Dealer hand is... "
   print "#{hand.first.display_rank} #{hand.first.suit} 🂠 \n"
+  give_space
 end
 
 def determine_winner(c_score, score)
   if @player_black_jack == "yes"
     puts "Player wins!"
+  elsif @computer_black_jack == "yes"
+    puts "Computer wins!"
+  elsif @busted_status == "yes" && @computer_busted == "yes"
+    puts "It's a draw."
   elsif @busted_status == "yes"
     puts "Computer wins!"
   elsif @computer_busted == "yes"
@@ -139,28 +160,41 @@ def determine_winner(c_score, score)
   end
 end
 
+def give_space
+  print "-" * 30
+  print "\n"
+end
+
 class PlayBlackjack
-  dance_with_em
-  deck = Deck.new
-  deck.shuffle_up
-  hand = []
-  dealer_hand = []
-  @go_again = "yes"
-  @busted_status = "no"
-  deal_cards(hand, deck)
-  deal_cards(dealer_hand, deck)
-  show_dealer_hand(dealer_hand)
-  display_hand(hand)
-  check_blackjack(hand, "Player")
-  puts calc_hand(hand)
-  while @busted_status == "no" && @go_again == "yes"
-    @player_score = calc_hand(hand)
-    make_a_move(hand, deck)
-    @player_score = calc_hand(hand)
+    loop do
+    dance_with_em
+    deck = Deck.new
+    deck.shuffle_up
+    hand = []
+    dealer_hand = []
+    @go_again = "yes"
+    @busted_status = "no"
+    deal_cards(hand, deck)
+    deal_cards(dealer_hand, deck)
+    show_dealer_hand(dealer_hand)
+    print "Players hand: "
     display_hand(hand)
-    puts calc_hand(hand)
-    check_busted(hand)
+    give_space
+    check_blackjack(hand, "Player")
+    puts "Player has: #{calc_hand(hand)}"
+    while @busted_status == "no" && @go_again == "yes"
+      @player_score = calc_hand(hand)
+      make_a_move(hand, deck)
+      give_space
+      @player_score = calc_hand(hand)
+      print "Players hand: "
+      display_hand(hand)
+      give_space
+      puts "Player has: #{calc_hand(hand)}"
+      give_space
+      check_busted(hand)
+    end
+    dealer_logic(dealer_hand, deck)
+    determine_winner(@dealer_score, @player_score)
   end
-  dealer_logic(dealer_hand, deck)
-  determine_winner(@dealer_score, @player_score)
 end
